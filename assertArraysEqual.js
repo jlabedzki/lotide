3d39b35ae0338dd3fea4adb92f173d6009321639
Johnny <johnnylabedzki@hotmail.com>
2021-08-17 09:30:13 -0700

    Created an assertion function to equate arrays.

---
+++ assertArraysEqual.js
@@ -0,0 +1,25 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let result = arr1.every(function (element, index) {
+    return element === arr2[index];
+  });
+
+  return result;
+};
+
+const assertArraysEqual = (actual, expected) => {
+  if (!Array.isArray(actual) || !Array.isArray(expected)) {
+    return console.log(`⛔️ Assertion Failed: One or more of the parameters is not an array.`);
+  }
+
+  let result = eqArrays(actual, expected);
+
+  result ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+assertArraysEqual([1, 2, 3], ['1', 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
