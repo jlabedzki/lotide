af6016786028f14261e03b6bdad60d84f4cc4a8e
Johnny <johnnylabedzki@hotmail.com>
2021-08-17 14:11:48 -0700

    Initial commit: created a function to bring nested arrays into a single-level array.

---
+++ flatten.js
@@ -0,0 +1,46 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let result = arr1.every(function (element, index) {
+    return element === arr2[index];
+  });
+
+  return result;
+};
+
+const assertArraysEqual = (actual, expected) => {
+
+  if (!Array.isArray(actual) || !Array.isArray(expected)) {
+    return console.log(`⛔️ Assertion Failed: One or more of the parameters is not an array.`);
+  }
+
+  let result = eqArrays(actual, expected);
+
+  if (result) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    return;
+  } else {
+    console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
+    return;
+  }
+
+};
+
+const flatten = (nestedArray) => {
+  let output = [];
+
+  for (let i = 0; i < nestedArray.length; i++) {
+    for (let j = 0; j < nestedArray[i].length; j++) {
+      output.push(nestedArray[i][j]);
+    }
+    if (!Array.isArray(nestedArray[i])) {
+      output.push(nestedArray[i]);
+    }
+  }
+
+  return output;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
