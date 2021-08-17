e5b0f7664ea4fd5f1cd2b5a501ff86d676090833
Johnny <johnnylabedzki@hotmail.com>
2021-08-17 08:44:25 -0700

    Created a function to equate arrays.

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function (actual, expected) {
+  actual === expected ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`⛔️ Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  let result = arr1.every(function (element, index) {
+    return element === arr2[index];
+  });
+
+  return result;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
