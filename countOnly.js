049e7116f3d77eeef3b18bfa447d017969e01f75
Johnny <johnnylabedzki@hotmail.com>
2021-08-17 19:33:30 -0700

    ESLinted the countOnly.js file

--- countOnly.js
+++ countOnly.js
@@ -1,13 +1,9 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   return actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}`
     : `⛔️ Assertion Failed: ${actual} !== ${expected}`;
 };
 
-//TODO Create a function that will take an array and an object as parameters. It will return an object containing counts of everything that the input object listed.
-
-//
-
-const countOnly = function (allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) {
   const results = {};
 
   for (const item of allItems) {
