9be99ae2e111a97a23447801fffe104ae021b933
Johnny <johnnylabedzki@hotmail.com>
2021-08-18 13:28:18 -0700

    Modified eqObjects.js to make us of my eqArrays function.

--- eqObjects.js
+++ eqObjects.js
@@ -17,24 +17,35 @@ const eqArrays = (arr1, arr2) => {
 
 const eqObjects = function(object1, object2) {
 
-  let keyValue1 = Object.entries(object1).sort().flat(Infinity);
-  let keyValue2 = Object.entries(object2).sort().flat(Infinity);
+  const key1 = Object.keys(object1);
+  const key2 = Object.keys(object2);
 
-  let length1 = keyValue1.length;
-  let length2 = keyValue2.length;
+  const length1 = key1.length;
+  const length2 = key2.length;
 
   if (length1 !== length2) {
     return false;
   }
 
-
-  for (let i = 0; i < keyValue1.length; i++) {
-    for (let j = 0; j < keyValue1[i].length; j++) {
-      if (keyValue1[i][j] !== keyValue2[i][j]) {
+  for (let i = 0; i < key1.length; i++) {
+    let keyName = key1[i];
+    if (Array.isArray(object1[keyName])) {
+      if (!eqArrays(object1[keyName], object2[keyName])) {
+        console.log('eqArray');
         return false;
+      } else {
+        continue;
       }
     }
+    if (object1[keyName] !== object2[keyName]) {
+      console.log(object1[keyName], object2[keyName]);
+      return false;
+    }
   }
+
+  // if (Array.isArray(object1[keyValue1[i][j]]) && Array.isArray(object1[keyValue2[i][j]]) && !eqArrays(object1[keyValue1[i][j]], object1[keyValue2[i][j]])) {
+  //   return false;
+  // }
 
   return true;
 };
@@ -46,9 +57,16 @@ console.log(assertEqual(eqObjects(ab, ba), true)); // => true
 const abc = { a: "1", b: "2", c: "3" };
 console.log(assertEqual(eqObjects(ab, abc), false)); // => false
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
+const cd = {
+  c: "2",
+  d: ["2", 3]
+};
+
+const dc = {
+  d: ["2", 3],
+  c: "1"
+};
+console.log(eqObjects(dc, cd)); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 console.log(eqObjects(cd, cd2)); // => false
