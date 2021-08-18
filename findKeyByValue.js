2b124d8b58827033ee20ca3ea9122f5e8e55e645
Johnny <johnnylabedzki@hotmail.com>
2021-08-17 20:46:11 -0700

    Created a function which takes an object and a value paramater and returns the first key that contains the given value.

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected) {
+  return actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}`
+    : `⛔️ Assertion Failed: ${actual} !== ${expected}`;
+};
+
+const findKeyByValue = function(object, value) {
+
+  // Converts an object to a nested array, where each array is a key-value pair.
+  const objToArray = Object.entries(object);
+
+  for (let i = 0; i < objToArray.length; i++) {
+    for (let j = 0; j < objToArray[i].length; j++) {
+      if (objToArray[i][j] === value) {
+        return objToArray[i][0];
+      }
+    }
+  }
+
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  'sci_fi': "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
