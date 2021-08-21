f397a0fecc1253a6a81abd2a35c9e086b471fa20
Johnny <johnnylabedzki@hotmail.com>
2021-08-20 21:34:22 -0700

    Refactored eqArrays function to use recursion for the case of infinite nested arrays.

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,28 @@
-const assertEqual = function (actual, expected) {
+const assertEqual = function(actual, expected) {
   return actual === expected ? `✅ Assertion Passed: ${actual} === ${expected}`
     : `⛔️ Assertion Failed: ${actual} !== ${expected}`;
 };
 
-const eqArrays = (arr1, arr2) => {
-  const result = arr1.every((el, ind) => el === arr2[ind]);
+//* Previous eqArrays function (still works with nested arrays)
+// const eqArrays = (arr1, arr2) => {
+//   const result = arr1.every((el, ind) => el === arr2[ind]);
+//   return arr1.length !== arr2.length ? false : result;
+// };
 
-  return arr1.length !== arr2.length ? false : result;
+//* New eqArrays function using recursion (built for the purpose of the exercise.)
+const eqArrays = (arr1, arr2) => {
+  const result = arr1.every((e, ind) => {
+    if (Array.isArray(e)) {
+      eqArrays(e, arr2[ind]);
+    } else {
+      return e === arr2[ind];
+    }
+  });
+
+  return result;
 };
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, [2, [1, 2, 3], 3]], [1, 2, [2, [2, 1, 3]], 3])); // => false
 console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
 
 console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
