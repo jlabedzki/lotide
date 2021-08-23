76e831abd28961816a53ffb2dd475fd39b1ef285
Johnny <johnnylabedzki@hotmail.com>
2021-08-23 08:23:56 -0700

    Created a package.json, installed mocha and chai, and refactored the headTest.js to use mocha and chai rather than my manual test code.

--- head.js
+++ head.js
@@ -1,3 +1,3 @@
-const head = array => array ? array[0] : undefined;
+const head = array => Array.isArray(array) ? array[0] : undefined;
 
 module.exports = head;
--- tests/headTest.js
+++ tests/headTest.js
@@ -1,7 +1,26 @@
-const assertEqual = require('../assertEqual');
-
+const assert = require('chai').assert;
 const head = require('../head');
 
-console.log(assertEqual(head([5, 6, 7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
-console.log(assertEqual(head(), "Johnny"));
+describe('#head', function () {
+  it(`should return the first element of an array`, function () {
+    assert.equal(head([1, 2, 3]), 1);
+  });
+
+  it(`should return the first element of an array even if there is only one element in the array`, function () {
+    assert.equal(head([5]), 5);
+  });
+
+  it(`should return undefined if an array is empty`, function () {
+    assert.equal(head([]), undefined);
+  });
+
+  it(`should return undefined if the argument passed is not an array`, function () {
+    assert.equal(head('Johnny'), undefined);
+  });
+});
+
+
+
+// console.log(assertEqual(head([5, 6, 7]), 5));
+// console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+// console.log(assertEqual(head(), "Johnny"));
